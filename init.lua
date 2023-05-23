85d9b98ed3ea38d9bc32f9dd83fffce7370095b8
ZephOne <zephone@protonmail.com>
2023-05-23 11:22:18 +0200

    Use terrafox as a colorscheme instead of nord

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "nord",
+  colorscheme = "terafox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
